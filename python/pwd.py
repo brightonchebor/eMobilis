f30dccf5ef0ec9f84b7cb728790bf5aedf7cf64d
brightonchebor <cheborbrighton805@gmail.com>
2024-10-25 08:26:57 +0300

    cn

---
+++ python/pwd.py
@@ -0,0 +1,7 @@
+predefined_pwd = 'sb62pu4142321'
+new_pwd = input('Enter your password: ')
+
+if predefined_pwd == new_pwd:
+    print('Access granted')
+else:
+    print('Access deneied')    
--- python/user_input.py
+++ python/user_input.py
@@ -2,4 +2,5 @@ f_name = input('Enter first name: ')
 l_name = input('Enter last name: ')
 age = int(input('Enter your age: '))
 
+print('')
 print(f'{l_name} {f_name}, {age}')
