40faf44198f541211bb43c75d158e509821104cf
brightonchebor <cheborbrighton805@gmail.com>
2024-10-28 14:01:12 +0300

    cn

---
+++ python/functions.py
@@ -0,0 +1,12 @@
+# def php_function():
+#     print('Hello php class students')
+
+# php_function()
+
+def calcSum():
+    num1 = int(input('Enter the first number: '))
+    num2 = int(input('Enter the second number'))
+
+    print(f'The sum is {num1 + num2}')
+
+calcSum()
