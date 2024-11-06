6eadaa01a8b04d36600fbfdf745d1048c3a2d03e
brightonchebor <cheborbrighton805@gmail.com>
2024-11-06 14:13:49 +0300

    cn

--- project_installation/myapp/views.py
+++ project_installation/myapp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
 
     return render(request, 'myapp/index.html', context={})
+
+def about_us(request):
+
+    return render(request, 'myapp/about_us.html', context={})
